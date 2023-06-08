ae11750f379a754435ed8b47dc9efd6834112ea2
Никита Мусатов <la3xmus@gmail.com>
2023-06-08 13:58:14 +0300

    Добавьте файлы проекта.

---
+++ CodeWars3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word1 = "world";
+            string word2 = "word";
+
+            char[] arr = word1.Reverse().ToArray();
+            Console.WriteLine(arr);
+            char[] barr = word2.Reverse().ToArray();
+            Console.WriteLine(barr);
+        }
+    }
+}
